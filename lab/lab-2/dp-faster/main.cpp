7e95205368ed51aa8e0cd783a1529a4d7dc8c94e
Yalfoosh <headsouldev@gmail.com>
2020-12-06 01:42:22 +0100

    Dodaj formatiranje za DP brže LAB-2

--- lab/lab-2/dp-faster/main.cpp
+++ lab/lab-2/dp-faster/main.cpp
@@ -5,18 +5,19 @@
 #define range 2000
 
 
-void printArray(uint16_t* problem, uint width, uint height) {
-    for(auto i = 0; i < height; ++i) {
-        for(auto j = 0; j < width; ++j)
+void printArray(uint16_t *problem, uint width, uint height) {
+    for (auto i = 0; i < height; ++i) {
+        for (auto j = 0; j < width; ++j)
             std::cout << problem[i * width + j] << " ";
 
         std::cout << std::endl;
     }
 }
 
-void print(uint16_t* problem, uint width, uint height, uint16_t* current) {
-    if(problem != current) {
-        if((current < problem + width) || (*(current - 1) < *(current - width)))
+void print(uint16_t *problem, uint width, uint height, uint16_t *current) {
+    if (problem != current) {
+        if ((current < problem + width) ||
+            (*(current - 1) < *(current - width)))
             print(problem, width, height, current - 1);
         else
             print(problem, width, height, current - width);
@@ -24,19 +25,21 @@ void print(uint16_t* problem, uint width, uint height, uint16_t* current) {
 
     auto offset = current - problem;
 
-    std::cout << "(" << offset / width << ", " << offset % width << ")" << std::endl;
+    std::cout << "(" << offset / width << ", " << offset % width << ")"
+              << std::endl;
 }
 
-void solve(uint16_t* problem, uint width, uint height) {
-    for(auto i = 1; i < height; ++i)
+void solve(uint16_t *problem, uint width, uint height) {
+    for (auto i = 1; i < height; ++i)
         problem[i * width] += problem[(i - 1) * width];
 
-    for(auto j = 1; j < width; ++j)
+    for (auto j = 1; j < width; ++j)
         problem[j] += problem[j - 1];
 
-    for(auto i = 1; i < height; ++i)
-        for(auto j = 1; j < width; ++j)
-            problem[i * width + j] += std::min(problem[(i - 1) * width + j], problem[i * width + j - 1]);
+    for (auto i = 1; i < height; ++i)
+        for (auto j = 1; j < width; ++j)
+            problem[i * width + j] += std::min(problem[(i - 1) * width + j],
+                                               problem[i * width + j - 1]);
 
     auto last = problem + width * height - 1;
 
@@ -50,11 +53,11 @@ int main() {
 
     auto problem = new uint16_t[M * N];
 
-    for(auto i = 0; i < M; ++i)
-        for(auto j = 0; j < N; ++j)
+    for (auto i = 0; i < M; ++i)
+        for (auto j = 0; j < N; ++j)
             problem[i * N + j] = rand() % range;
 
-    if(printTheArray) {
+    if (printTheArray) {
         printArray(problem, N, M);
         std::cout << std::endl;
     }
